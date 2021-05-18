77876cd2467486b9ac8da85ea05cf8c245f2b3c9
Rajat Goyal <i.m.rajatgoyal@gmail.com>
2021-05-18 18:38:16 +0530

    "User entity updated"

--- src/main/java/com/root/entity/User.java
+++ src/main/java/com/root/entity/User.java
@@ -1,5 +1,27 @@
 package com.root.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Users")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int user_id;
+	
+	@Column(unique = true, nullable = false)
+	private String user_name;
+	
+	@Column(unique = true, nullable = false)
+	private String password;
+	
+	@Column(nullable = false, columnDefinition = "user")
+	private String admin_role;
 
 }
