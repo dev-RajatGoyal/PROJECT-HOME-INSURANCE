f7b73ea8eab4cbabcde42b037e9d79bf16e03d77
Rajat Goyal <i.m.rajatgoyal@gmail.com>
2021-05-19 14:13:25 +0530

    "server config successful"

--- src/main/java/com/root/controller/Demo.java
+++ src/main/java/com/root/controller/Demo.java
@@ -2,15 +2,16 @@ package com.root.controller;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import com.root.entity.User;
-import com.root.utility.JPAUtility;
 
 public class Demo {
 	public static void main(String[] args) {
 		
-		EntityManagerFactory efm = JPAUtility.getEntityManagerFactory();
-		EntityManager em = efm.createEntityManager();
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit1");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
 		User user = new User();
 		user.setUser_id(1);
@@ -18,11 +19,12 @@ public class Demo {
 		user.setPassword("abc");
 		user.setRole("user");
 		
-		em.getTransaction().begin();
-		System.out.println("USer stored successfully");
-		em.persist(user);
+		entityManager.getTransaction().begin();
+		entityManager.persist(user);
+		entityManager.getTransaction().commit();
 		
-		em.getTransaction().commit();
-		
+		System.out.println("success");
+		entityManager.close();
+		entityManagerFactory.close();
 	}
 }
